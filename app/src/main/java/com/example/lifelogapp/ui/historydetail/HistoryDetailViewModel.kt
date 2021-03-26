100cd8fff2cfe5e77f313ca94e0698e51dcfff6e
wataruyoshida <watarutower@hotmail.com>
2021-03-26 21:29:32 +0900

    動きます

--- app/src/main/java/com/example/lifelogapp/ui/historydetail/HistoryDetailViewModel.kt
+++ app/src/main/java/com/example/lifelogapp/ui/historydetail/HistoryDetailViewModel.kt
@@ -2,12 +2,15 @@ package com.example.lifelogapp.ui.historydetail
 
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lifelogapp.database.Lifelog
 import com.example.lifelogapp.database.LifelogDao
 import com.example.lifelogapp.formatDaylogs
+import kotlinx.coroutines.launch
 
 class HistoryDetailViewModel (
         private val dayLogsKey: String = "",
@@ -21,10 +24,18 @@ class HistoryDetailViewModel (
 
     val theDay = dayLogsKey
 
-
+    val editWrite =""
+//
+//    fun onWriteClicked() {
+//        viewModelScope.launch {
+//            invalidateAll()
+//            editWrite.visibility = View.VISIBLE
+//
+//        }
 
 //    val daylogsString = Transformations.map(daylog) { daylogs ->
 //        formatDaylogs(daylogs, application.resources)
 //    }
-}
+    }
+
 
